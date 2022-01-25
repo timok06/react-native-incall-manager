8019a1eac7a7de61a50b91d6def4251d01f0c853
timok06 <t.koehler@enna.care>
2022-01-25 11:55:12 +0100

    added audioManager.setMode(Mode_IN_COMMUNICATION) in setSpeakerPhoneOn

--- android/src/main/java/com/zxcpoiu/incallmanager/InCallManagerModule.java
+++ android/src/main/java/com/zxcpoiu/incallmanager/InCallManagerModule.java
@@ -862,6 +862,7 @@ public class InCallManagerModule extends ReactContextBaseJavaModule implements L
     public void setSpeakerphoneOn(final boolean enable) {
         if (enable != audioManager.isSpeakerphoneOn())  {
             Log.d(TAG, "setSpeakerphoneOn(): " + enable);
+            audioManager.setMode(AudioManager.MODE_IN_COMMUNICATION);
             audioManager.setSpeakerphoneOn(enable);
         }
     }
